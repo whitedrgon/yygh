f8cb6fdf1ba0bd35cd49bc766ae9130ccc162669
白玉京 <1150909122@qq.com>
2023-04-03 17:29:55 +0800

    hot-fix-yygh-first-commit

--- yygh_parent/service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
+++ yygh_parent/service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
@@ -36,6 +36,7 @@ public class DictController {
     @PostMapping("/upload")
     public R upload(MultipartFile file) throws IOException {
         dictService.upload(file);
+        System.out.println("我是hot-fix分支");
         return R.ok();
     }
     @ApiOperation(value = "excel下载")
